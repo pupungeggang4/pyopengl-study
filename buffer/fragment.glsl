a945f35c7270ae5125423f8d1f1835953f2f18cc
pupungeggang2 <kyongkyong2892@gmail.com>
2025-07-07 01:32:47 +0900

    temp

---
+++ buffer/fragment.glsl
@@ -0,0 +1,7 @@
+#version 330 core
+precision highp float;
+out vec4 o_color;
+
+void main() {
+    o_color = vec4(0.0, 1.0, 0.0, 1.0)
+}
---
+++ buffer/vertex.glsl
@@ -0,0 +1,7 @@
+#version 330 core
+
+in vec3 a_position;
+
+void main() {
+    gl_Position = a_position;
+}
