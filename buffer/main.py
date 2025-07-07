17289e18e53b446e2b235bd5b7d884414fb1bb16
pupungeggang2 <kyongkyong2892@gmail.com>
2025-07-07 16:22:27 +0900

    buffer finished

--- buffer/main.py
+++ buffer/main.py
@@ -8,6 +8,12 @@ from OpenGL.GL import *
 
 class Program():
     def __init__(self):
+        pg.init()
+        self.clock = pg.time.Clock()
+        self.FPS = 60
+        self.gl_init()
+
+    def gl_init(self):
         glfw.init()
         glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
         glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 1)
@@ -16,15 +22,7 @@ class Program():
         self.window = glfwCreateWindow(1280, 720, 'buffer test', None, None)
         glfwSetKeyCallback(self.window, self.key_callback)
         self.context = glfw.make_context_current(self.window)
-        self.gl_init()
 
-    def gl_init(self):
-        if 'WAYLAND_DISPLAY' in os.environ:
-            print("Running on Wayland.")
-        elif 'DISPLAY' in os.environ:
-            print("Running on X11.")
-        else:
-            print("Could not determine display server (possibly TTY).")
         f = open('vertex.glsl')
         self.v_shader_source = f.read()
         f.close()
@@ -52,22 +50,28 @@ class Program():
 
         self.vao = 1
         glGenVertexArrays(1, self.vao)
-        self.buffer = 1
-        glGenBuffers(1, self.buffer)
+        self.b = 1
+        glGenBuffers(1, self.b)
         glBindVertexArray(self.vao)
-        glBindBuffer(GL_ARRAY_BUFFER, self.buffer)
+        glBindBuffer(GL_ARRAY_BUFFER, self.b)
         glBufferData(GL_ARRAY_BUFFER, 4 * 9, np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0], dtype = np.float32), GL_STATIC_DRAW)
         glVertexAttribPointer(self.location['a_position'], 3, GL_FLOAT, GL_FALSE, 3 * 4, ctypes.c_void_p(0 * 4))
         glEnableVertexAttribArray(self.location['a_position'])
+        glViewport(0, 0, 1280, 720)
+
+        self.a = 0.0
 
     def run(self):
         while not glfw.window_should_close(self.window):
+            self.clock.tick
+            self.a += 0.01
             glClearColor(1.0, 1.0, 1.0, 1.0)
             glClear(GL_COLOR_BUFFER_BIT)
             glUseProgram(self.program)
             glEnableVertexAttribArray(self.location['a_position'])
             glBindVertexArray(self.vao)
-            glBindBuffer(GL_ARRAY_BUFFER, self.buffer)
+            glBindBuffer(GL_ARRAY_BUFFER, self.b)
+            glBufferData(GL_ARRAY_BUFFER, 4 * 9, np.array([0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0 - self.a, 0.0], dtype = np.float32), GL_STATIC_DRAW)
             glDrawArrays(GL_TRIANGLES, 0, 3)
             glfwSwapBuffers(self.window)
             glfw.poll_events()
